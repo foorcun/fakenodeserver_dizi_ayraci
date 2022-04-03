3d23240db1e70025c19c1cde8c931633a896d2be
foorcun <msnifoorcun@hotmail.com>
2022-04-03 21:25:42 +0300

    data post ile database e ekleniyor ama kendiliğinden güncellenmiş hali ekrana vermiyor

--- lib/features/dizi_ayraci/presentation/pages/add_dizi/add_dizi_view.dart
+++ lib/features/dizi_ayraci/presentation/pages/add_dizi/add_dizi_view.dart
@@ -1,14 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:http/http.dart' as http;
 
 class AddDiziView extends StatelessWidget {
-  const AddDiziView({Key? key}) : super(key: key);
+  AddDiziView({Key? key}) : super(key: key);
+
+  final _diziTextController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      // floatingActionButton: FloatingActionButton(
+      //   onPressed: _floatingPrint,
+      //   tooltip: 'Add new Dizi',
+      //   child: const Icon(Icons.add),
+      // ),
       body: Center(
-        child: Text("Add dizi view"),
+        child: Column(
+          children: [
+            Container(
+              child: TextField(
+                controller: _diziTextController,
+              ),
+              padding: EdgeInsets.all(32),
+            ),
+            ElevatedButton(
+              onPressed: () async {
+                print(_diziTextController.text);
+                var uri = Uri.parse("http://localhost:3000/diziler");
+                var response = await http.post(uri, body: {
+                  "diziName": _diziTextController.text,
+                });
+                print(response.body);
+
+                // Get.back();
+                Get.toNamed("/main_screen_getx");
+              },
+              child: Text("Yeni Dizi Yarat"),
+            ),
+          ],
+        ),
       ),
     );
   }
+
+  // void _floatingPrint() {
+  //   // Get.toNamed("/AddDiziView");
+  //   Get.back();
+  // }
 }
