ffb864ab3c06c5b0516087e2a087ab2497d40234
foorcun <msnifoorcun@hotmail.com>
2022-04-03 21:45:00 +0300

    Slidable eklendi

--- lib/features/dizi_ayraci/presentation/pages/main_screen_getx.dart
+++ lib/features/dizi_ayraci/presentation/pages/main_screen_getx.dart
@@ -4,6 +4,8 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
+import 'package:flutter_slidable/flutter_slidable.dart';
+
 class MainScreenGetx extends StatelessWidget {
   MainScreenGetx({Key? key}) : super(key: key);
 
@@ -28,22 +30,70 @@ class MainScreenGetx extends StatelessWidget {
           ListView.builder(
               itemCount: Get.find<DiziController>().diziler.length,
               itemBuilder: (context, index) {
-                return Card(
-                  child: ListTile(
-                    title: Text(
-                        Get.find<DiziController>().diziler[index].diziName),
-                    onTap: () {
-                      print("index " + index.toString());
-                      print("Get.find<DiziController>().diziler[index].id " +
-                          Get.find<DiziController>()
-                              .diziler[index]
-                              .id
-                              .toString());
-                      Get.find<SezonController>().setSecilenDiziId(
-                          Get.find<DiziController>().diziler[index].id);
+                return Slidable(
+                  child: Card(
+                    child: ListTile(
+                      title: Text(
+                          Get.find<DiziController>().diziler[index].diziName),
+                      onTap: () {
+                        print("index " + index.toString());
+                        print("Get.find<DiziController>().diziler[index].id " +
+                            Get.find<DiziController>()
+                                .diziler[index]
+                                .id
+                                .toString());
+                        Get.find<SezonController>().setSecilenDiziId(
+                            Get.find<DiziController>().diziler[index].id);
 
-                      Get.toNamed("/SezonlarByDizi");
-                    },
+                        Get.toNamed("/SezonlarByDizi");
+                      },
+                    ),
+                  ),
+                  endActionPane: ActionPane(
+                    motion: const ScrollMotion(),
+                    children: [
+                      SlidableAction(
+                        flex: 2,
+                        onPressed: (BuildContext context) {
+                          print("basıldım...");
+
+                          showDialog(
+                              context: context,
+                              builder: (_) {
+                                return CupertinoAlertDialog(
+                                  content: Text(Get.find<DiziController>()
+                                          .diziler[index]
+                                          .diziName +
+                                      " silmek ister misiniz?"),
+                                  actions: [
+                                    CupertinoDialogAction(
+                                      child: ElevatedButton(
+                                        onPressed: () {
+                                          // Navigator.pop(context);
+                                          Get.back();
+                                        },
+                                        child: Text("İptal"),
+                                      ),
+                                    ),
+                                    CupertinoDialogAction(
+                                      child: ElevatedButton(
+                                        child: Text("Evet"),
+                                        onPressed: () async {
+                                          // await cDizi.delete();
+                                          Get.back();
+                                        },
+                                      ),
+                                    )
+                                  ],
+                                );
+                              });
+                        },
+                        backgroundColor: Colors.red,
+                        foregroundColor: Colors.white,
+                        icon: Icons.delete_forever,
+                        label: 'Delete',
+                      ),
+                    ],
                   ),
                 );
               })),
